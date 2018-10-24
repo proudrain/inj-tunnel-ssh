0936de4c01a25ce829b8ef3440f2b6213f50b93a
Saul-Mirone <xy610888871@gmail.com>
2018-10-24 11:05:44 +0800

    fix dts port bug, optimise example ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,17 @@
 import * as events from 'events';
 
-type _Config = Partial<{
-  dstHost: string;
-  dstPort: number;
-  username: string;
-  password: string;
-  host: string;
-  port: string;
-}>;
-
 export as namespace tInjectSSH;
 
 export = tinject;
 
-declare function tinject(config: Array<_Config>): events.EventEmitter;
+declare function tinject(config: Array<tinject.Config>): events.EventEmitter;
 declare namespace tinject {
-  export type Config = _Config;
+  export type Config = Partial<{
+    dstHost: string;
+    dstPort: number;
+    username: string;
+    password: string;
+    host: string;
+    port: number;
+  }>;
 }
