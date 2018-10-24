a1a7ab9ff1ba617e5e22e56b93dd28637ebcb658
Mirone <xy610888871@gmail.com>
2018-10-24 11:10:18 +0800

    dts bug fix, optimise dts example (#2)
    * fix dts port bug, optimise example ts

--- example/default.ts
+++ example/default.ts
@@ -3,11 +3,15 @@ import * as tinject from '../lib';
 const config: tinject.Config = {
   dstPort: 27017,
   host: 'tunneltest1.com',
-  username: 'root'
+  username: 'root',
+  port: 8080
 };
 
 tinject([config, {
     dstPort: 27017,
+    dstHost: 'localhost',
     host: 'tunneltest2.com',
-    username: 'root'
+    port: 9001,
+    username: 'root',
+    password: '123456'
 }]);
