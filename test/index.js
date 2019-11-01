befa76cb3af904542d21927ca0dc3e09392d4521
proudrain <lyhproud@163.com>
2019-11-01 18:08:29 +0800

    Fix code style

--- test/index.js
+++ test/index.js
@@ -4,10 +4,6 @@ var ts = require('proxy-sinon-chai');
 var expect = ts.expect;
 
 describe('inject-tunnel-ssh', function() {
-
-    beforeEach(() => {
-    });
-
     it('should return a port number', function(done) {
         portscan(8080, function(error, port){
            expect(port).to.be.at.least(8080);
